9dd5d3be3e00de99c7d7a98c683b1a91a8883c5a
jesse <jesse.deng@deltaping.com>
2026-06-11 17:32:18 +0800

    Revert "Add event emission for collected fees in allocation process"
    This reverts commit 8660e6da8a1c2da465984f3e4cb07ad9f5e2d8bf.

--- x/distribution/types/events.go
+++ x/distribution/types/events.go
@@ -2,17 +2,14 @@ package types
 
 // distribution module event types
 const (
-	EventTypeSetWithdrawAddress    = "set_withdraw_address"
-	EventTypeRewards               = "rewards"
-	EventTypeCommission            = "commission"
-	EventTypeWithdrawRewards       = "withdraw_rewards"
-	EventTypeWithdrawCommission    = "withdraw_commission"
-	EventTypeProposerReward        = "proposer_reward"
-	EventTypeTransferCollectedFees = "transfer_collected_fees"
+	EventTypeSetWithdrawAddress = "set_withdraw_address"
+	EventTypeRewards            = "rewards"
+	EventTypeCommission         = "commission"
+	EventTypeWithdrawRewards    = "withdraw_rewards"
+	EventTypeWithdrawCommission = "withdraw_commission"
+	EventTypeProposerReward     = "proposer_reward"
 
 	AttributeKeyWithdrawAddress = "withdraw_address"
-	AttributeKeySenderModule    = "sender_module"
-	AttributeKeyRecipientModule = "recipient_module"
 	AttributeKeyValidator       = "validator"
 	AttributeKeyDelegator       = "delegator"
 )
